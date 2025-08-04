d184dffaa7a1ce916c0cacb43676c4586f31296c
Jeff <jefrife@gmail.com>
2025-08-04 15:03:51 +0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {RouterOutlet} from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { MessageModule } from 'primeng/message';
+import { CommonModule } from '@angular/common';
+import {PanelModule} from "primeng/panel";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, InputTextModule, ButtonModule, MessageModule, FormsModule, PanelModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todo-list';
+  text = '';
+
+  msg = '';
+
+  onClick() {
+    this.msg = 'Welcome ' + this.text;
+  }
 }
